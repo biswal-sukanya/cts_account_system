211164d01926f931e42f546a26d811fe2b810efa
administrator <administrator@iispl>
2026-08-04 14:37:00 +0530

    added account balance validation and removed status validation

--- src/com/iispl/validator/AccountBalanceValidationRule.java
+++ src/com/iispl/validator/AccountBalanceValidationRule.java
@@ -1,13 +1,18 @@
 package com.iispl.validator;
 
+import java.math.BigDecimal;
+
+import com.iispl.exception.InvalidAccountBalanceException;
 import com.iispl.model.Account;
 
 public class AccountBalanceValidationRule implements AccountValidator{
 
 	@Override
-	public void validate(Account account) {
+	public void validate(Account account) throws InvalidAccountBalanceException {
 		
-		
+		if(account.getAccountBalance().compareTo(BigDecimal.ZERO)<=0) {
+			throw new InvalidAccountBalanceException();
+		}
 	}
 	
 }
--- src/com/iispl/validator/AccountStatusValidationRule.java
+++
@@ -1,5 +0,0 @@
-package com.iispl.validator;
-
-public class AccountStatusValidationRule {
-
-}
